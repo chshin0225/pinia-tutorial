8751f39a43dd419d25de4276a6573993620204de
Christine Chaewon Shin <chshin0225@gmail.com>
2023-08-24 23:26:36 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Task</h1>
+    </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import TaskDetails from './components/TaskDetails.vue'
+import { useTaskStore } from './stores/TaskStore'
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore()
+
+    return {
+      taskStore,
+    }
+  }
+}
+</script>
