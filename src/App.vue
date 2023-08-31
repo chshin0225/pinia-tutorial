96673a80f0fb68a97377b913103cf3c69adb43c9
Christine Chaewon Shin <chshin0225@gmail.com>
2023-08-31 20:23:04 +0900

    create task list using pinia

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,65 @@
       <h1>Pinia Task</h1>
     </header>
 
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <!-- loading -->
+    <div class="loading" v-if="loading">Loading tasks...</div>
+
     <!-- task list -->
-    <div class="task-list">
-      <div v-for="task in taskStore.tasks">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ favCount }} tasks left to do</p>
+      <div v-for="task in favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <button @click="taskStore.$reset">reset</button>
   </main>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
 import TaskDetails from './components/TaskDetails.vue'
 import { useTaskStore } from './stores/TaskStore'
+import TaskForm from './components/TaskForm.vue'
 
 export default {
-  components: { TaskDetails },
+  components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore()
 
+    const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore)
+
+    // fetch tasks
+    taskStore.getTasks()
+
+    const filter = ref('all')
+
     return {
       taskStore,
+      filter,
+      tasks,
+      loading,
+      favs,
+      totalCount,
+      favCount,
     }
   }
 }
